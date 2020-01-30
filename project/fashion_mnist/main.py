d436641f927db3abc496ec5187cbee7d005fefb0
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-01-30 23:25:09 +0100

    Basic dataset api support added 91.48%

--- project/fashion_mnist/main.py
+++ project/fashion_mnist/main.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import numpy as np
 from fashion_mnist.callbacks.get_callbacks import get_callbacks
+from fashion_mnist.dataset.data_api import get_train_data
+from fashion_mnist.dataset.data_api import get_validation_data
+from sklearn.model_selection import train_test_split 
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
-
 train_images = np.expand_dims(train_images, axis = -1)/255
 test_images = np.expand_dims(test_images, axis = -1)/255
+
+train_images, validation_images, train_labels, validation_labels = train_test_split(train_images, train_labels, test_size = 0.15, random_state = 42, shuffle = True)
 
 inputLayer = tf.keras.layers.Input(shape = (28, 28, 1))
 x = inputLayer
@@ -20,8 +24,9 @@ output = tf.keras.layers.Dense(10, activation = 'softmax')(x)
 
 model = tf.keras.Model(inputs = inputLayer, outputs = output)
 model.compile('adam', loss = 'sparse_categorical_crossentropy', metrics = ['sparse_categorical_crossentropy', 'accuracy'])
-model.fit(x=train_images, y=train_labels, batch_size=128, epochs=50, verbose=1, callbacks=get_callbacks(), validation_split=0.15, shuffle=True)
+model.fit(x=get_train_data(train_images, train_labels), validation_data=get_validation_data(validation_images, validation_labels), epochs=50, verbose=1, callbacks=get_callbacks())
 
 model1 = tf.keras.models.load_model('./checkpoints/cp.ckpt')
+model1.summary()
 result = model1.evaluate(x=test_images, y=test_labels, batch_size=32)
 print('Test:\n Sparse_categorical_crossentropy: {}\n Accuracy: {}'.format(result[1], result[2]))
