bce270396277cd52d05e46e742dd5cd06aa4059c
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-01-30 22:38:02 +0100

    changes to enable normal training 91% on test

--- project/fashion_mnist/main.py
+++ project/fashion_mnist/main.py
@@ -4,8 +4,8 @@ from fashion_mnist.callbacks.get_callbacks import get_callbacks
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
-train_images = np.expand_dims(train_images, axis = -1)
-test_images = np.expand_dims(test_images, axis = -1)
+train_images = np.expand_dims(train_images, axis = -1)/255
+test_images = np.expand_dims(test_images, axis = -1)/255
 
 inputLayer = tf.keras.layers.Input(shape = (28, 28, 1))
 x = inputLayer
@@ -15,12 +15,12 @@ for i in range(2):
     x = tf.keras.layers.MaxPool2D()(x)
 
 x = tf.keras.layers.Flatten()(x)
-x = tf.keras.layers.Dense(512, activation = 'relu')(x)
+x = tf.keras.layers.Dense(256, activation = 'relu')(x)
 output = tf.keras.layers.Dense(10, activation = 'softmax')(x)
 
 model = tf.keras.Model(inputs = inputLayer, outputs = output)
 model.compile('adam', loss = 'sparse_categorical_crossentropy', metrics = ['sparse_categorical_crossentropy', 'accuracy'])
-model.fit(x=train_images, y=train_labels, batch_size=32, epochs=2, verbose=1, callbacks=get_callbacks(), validation_split=0.15, shuffle=True)
+model.fit(x=train_images, y=train_labels, batch_size=128, epochs=50, verbose=1, callbacks=get_callbacks(), validation_split=0.15, shuffle=True)
 
 model1 = tf.keras.models.load_model('./checkpoints/cp.ckpt')
 result = model1.evaluate(x=test_images, y=test_labels, batch_size=32)
