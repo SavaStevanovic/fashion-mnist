223ceb7a447aa231a57b9c830bacbbd8e6804481
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-02-01 18:17:28 +0100

    Small resnet fix and new 94.51% with separable Resnet. Resnet needs reevaluation.

--- project/fashion_mnist/models/separable_resnet_model.py
+++ project/fashion_mnist/models/separable_resnet_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def get_model():
     inputLayer = tf.keras.layers.Input(shape = (28, 28, 1))
-    net = tf.keras.layers.Conv2D(filters=64, kernel_size=7, activation='relu', padding='SAME', use_bias=False)(inputLayer)
+    net = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu', padding='SAME', use_bias=False)(inputLayer)
     net = tf.keras.layers.BatchNormalization()(net)
     net = tf.keras.layers.Activation('relu')(net)
 
@@ -19,17 +19,17 @@ def get_model():
     net = tf.keras.layers.Flatten()(net)
     output = tf.keras.layers.Dense(10, activation = 'softmax')(net)
 
-    return tf.keras.Model(inputs = inputLayer, outputs = output, name = 'resnet_model')
+    return tf.keras.Model(inputs = inputLayer, outputs = output, name = 'separable_resnet_model')
     
 def residual_block(net, filters):
     if net.shape[-1].value != filters:
         net_pre = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, padding='SAME')(net)
     else:
         net_pre = net
-    net = tf.keras.layers.Conv2D(filters=filters, kernel_size=3, padding='SAME', use_bias=False)(net)
+    net = tf.keras.layers.SeparableConv2D(filters=filters, kernel_size=3, padding='SAME', use_bias=False)(net)
     net = tf.keras.layers.BatchNormalization()(net)
     net = tf.keras.layers.Activation('relu')(net)
-    net = tf.keras.layers.Conv2D(filters=filters, kernel_size=3, padding='SAME', use_bias=False)(net)
+    net = tf.keras.layers.SeparableConv2D(filters=filters, kernel_size=3, padding='SAME', use_bias=False)(net)
     net = tf.keras.layers.BatchNormalization()(net)
     net = tf.keras.layers.add([net, net_pre])
     net = tf.keras.layers.Activation('relu')(net)
