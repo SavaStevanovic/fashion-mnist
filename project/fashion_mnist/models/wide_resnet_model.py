95394250e4477c7f92b80e65bf22999f197d7a56
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-02-02 00:22:02 +0100

    Wide resnet made smaller and bether but training is slow so I will not optimise further

--- project/fashion_mnist/models/wide_resnet_model.py
+++ project/fashion_mnist/models/wide_resnet_model.py
@@ -4,8 +4,8 @@ def get_model():
     inputLayer = tf.keras.layers.Input(shape = (28, 28, 1))
     net = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu', padding='SAME')(inputLayer)
     
-    k = 4
-    rens_net_blocks = 4
+    k = 6
+    rens_net_blocks = 3
     for i in range(rens_net_blocks):
         net = residual_block(net, filters = k*16*2**i)
         net = residual_block(net, filters = k*16*2**i)
@@ -18,7 +18,7 @@ def get_model():
     net = tf.keras.layers.Flatten()(net)
     output = tf.keras.layers.Dense(10, activation = 'softmax')(net)
 
-    return tf.keras.Model(inputs = inputLayer, outputs = output, name = 'resnet_model')
+    return tf.keras.Model(inputs = inputLayer, outputs = output, name = 'wide_resnet_model')
     
 def residual_block(net, filters):
     if net.shape[-1].value != filters:
