248ca1c6985768ab69b73f6a708a05e32c9f89ef
Sava Stevanovic <sava.stevanovic@pmf.edu.rs>
2020-02-03 11:55:54 +0100

    fixes and evaluations of models

--- project/fashion_mnist/models/separable_wide_resnet_model.py
+++ project/fashion_mnist/models/separable_wide_resnet_model.py
@@ -2,16 +2,21 @@ import tensorflow as tf
 
 def get_model():
     inputLayer = tf.keras.layers.Input(shape = (28, 28, 1))
-    net = tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu', padding='SAME')(inputLayer)
+    net = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='SAME', use_bias=False)(inputLayer)
     
-    k = 6
+    k = 4
     rens_net_blocks = 3
+    layers_per_block = 6
     for i in range(rens_net_blocks):
-        net = residual_block(net, filters = k*16*2**i)
-        net = residual_block(net, filters = k*16*2**i)
+        for j in range(layers_per_block):
+            net = residual_block(net, filters = k*16*2**i)
         if i+1==rens_net_blocks:
+            net = tf.keras.layers.BatchNormalization()(net)
+            net = tf.keras.layers.Activation('relu')(net)
             net = tf.keras.layers.AveragePooling2D()(net)
         else:
+            net = tf.keras.layers.BatchNormalization()(net)
+            net = tf.keras.layers.Activation('relu')(net)
             net = tf.keras.layers.MaxPool2D()(net)
 
     net = tf.keras.layers.Flatten()(net)
@@ -24,9 +29,11 @@ def residual_block(net, filters):
         net_pre = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, padding='SAME')(net)
     else:
         net_pre = net
-    net = tf.keras.layers.SeparableConv2D(filters=filters, kernel_size=3, padding='SAME', activation = 'relu')(net)
-    net = tf.keras.layers.Dropout(rate = 0.2, seed = 42)(net)
-    net = tf.keras.layers.SeparableConv2D(filters=filters, kernel_size=3, padding='SAME', activation = 'relu')(net)
-    net = tf.keras.layers.add([net, net_pre])
-    return net
+    net = tf.keras.layers.BatchNormalization()(net)
+    net = tf.keras.layers.Activation('relu')(net)
+    net = tf.keras.layers.SeparableConv2D(filters=filters, kernel_size=3, padding='SAME', use_bias=False)(net)
+    net = tf.keras.layers.BatchNormalization()(net)
+    net = tf.keras.layers.Activation('relu')(net)
+    net = tf.keras.layers.SeparableConv2D(filters=filters, kernel_size=3, padding='SAME', use_bias=False)(net)
+    return tf.keras.layers.add([net, net_pre])
 
